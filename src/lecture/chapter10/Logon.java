b4bb8306577905ac0c891a7b46cbbe356397c44f
Matthias Berg-Neels <matthias.berg-neels@sap.com>
2025-07-03 20:43:42 +0200

    Action & MouseListener, MenuBars

--- src/lecture/chapter10/Logon.java
+++ src/lecture/chapter10/Logon.java
@@ -12,6 +12,9 @@ import java.util.Map;
 
 public class Logon extends JFrame {
 
+  private static final String ACTON_LOGIN = "ACTON_LOGIN";
+  private static final String ACTION_BEENDEN = "ACTION_BEENDEN";
+  private static final String ACTION_PRINT = "ACTION_PRINT";
   // Instanzattribute
   private final JFormattedTextField portField;
 
@@ -119,8 +122,78 @@ public class Logon extends JFrame {
 
     // create & assign Buttons
     JButton okButton = new JButton("Login");
+    okButton.setActionCommand(ACTON_LOGIN);
     JButton cancelButton = new JButton("Beenden");
+    cancelButton.setActionCommand(ACTION_BEENDEN);
     JButton printButton = new JButton("Ausgabe");
+    printButton.setActionCommand(ACTION_PRINT);
+
+    ActionListener buttonListener = e -> {
+      System.out.println("Action Event Command: " + e.getActionCommand());
+      System.out.println("Parameter String: " + e.paramString());
+      System.out.println("Timestamp (when): " + e.getWhen());
+      System.out.println("Modifiers: " + e.getModifiers());
+      System.out.println("Modifiers (binär): " + Integer.toBinaryString(e.getModifiers()));
+
+
+      if(e.getActionCommand().equals(ACTON_LOGIN)) {
+        System.out.println("OK Button gedrückt");
+        messageTextLabel.setText("Login erfolgreich!");
+
+        if((e.getModifiers() & ActionEvent.CTRL_MASK) == ActionEvent.CTRL_MASK){
+          System.out.println("CTRL gedrückt - Login wird nicht durchgeführt.");
+        }
+      } else if(e.getActionCommand().equals(ACTION_PRINT)) {
+        System.out.println("Print Button gedrückt");
+        messageTextLabel.setText("Ausgabe der Daten:");
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("User", userNameField.getText());
+        dataMap.put("Passwort", new String(userPasswordField.getPassword()));
+        dataMap.put("Art", (String) myComboBox.getSelectedItem());
+        dataMap.put("Host", host.getText());
+        dataMap.put("Port", portField.getText());
+        System.out.println(dataMap);
+      } else if(e.getActionCommand().equals(ACTION_BEENDEN)) {
+        System.out.println("Cancel Button gedrückt - Programm wird beendet.");
+        messageTextLabel.setText("Programm wird beendet.");
+        System.exit(0);
+      }
+    };
+
+    okButton.addActionListener(buttonListener);
+    printButton.addActionListener(buttonListener);
+    cancelButton.addActionListener(buttonListener);
+
+    okButton.addMouseListener(new MouseListener() {
+      @Override
+      public void mouseClicked(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        JButton button = (JButton) e.getSource();
+        button.setEnabled(false);
+        button.setText("Geh weg!");
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+        JButton button = (JButton) e.getSource();
+        button.setEnabled(true);
+        button.setText("Login");
+      }
+    });
 
     southPanel.add(okButton);
     southPanel.add(cancelButton);
@@ -146,6 +219,47 @@ public class Logon extends JFrame {
 
     this.add(mainPanel);
 
+    // Menüleiste hinzufügen
+    JMenuBar swingMenuBar = new JMenuBar();
+    JMenu swingMenu = new JMenu("Files");
+
+    JMenuItem swingLoginMenuItem = new JMenuItem("Login");
+    swingLoginMenuItem.setActionCommand(ACTON_LOGIN);
+    swingLoginMenuItem.addActionListener(buttonListener);
+
+    JMenuItem swingPrintMenuItem = new JMenuItem("Ausgabe");
+    swingPrintMenuItem.setActionCommand(ACTION_PRINT);
+    swingPrintMenuItem.addActionListener(buttonListener);
+
+    JMenuItem swingCancelMenuItem = new JMenuItem("Beenden");
+    swingCancelMenuItem.setActionCommand(ACTION_BEENDEN);
+    swingCancelMenuItem.addActionListener(buttonListener);
+
+    swingMenu.add(swingLoginMenuItem);
+    swingMenu.add(swingPrintMenuItem);
+    swingMenu.addSeparator();
+    swingMenu.add(swingCancelMenuItem);
+    swingMenuBar.add(swingMenu);
+    this.setJMenuBar(swingMenuBar);
+
+    MenuBar awtMenuBar = new MenuBar();
+    Menu fileMenu = new Menu("File");
+    MenuItem loginMenuItem = new MenuItem("Login");
+    loginMenuItem.setActionCommand(ACTON_LOGIN);
+    loginMenuItem.addActionListener(buttonListener);
+    MenuItem printMenuItem = new MenuItem("Ausgabe");
+    printMenuItem.setActionCommand(ACTION_PRINT);
+    printMenuItem.addActionListener(buttonListener);
+    MenuItem cancelMenuItem = new MenuItem("Beenden");
+    cancelMenuItem.setActionCommand(ACTION_BEENDEN);
+    cancelMenuItem.addActionListener(buttonListener);
+    fileMenu.add(loginMenuItem);
+    fileMenu.add(printMenuItem);
+    fileMenu.addSeparator();
+    fileMenu.add(cancelMenuItem);
+    awtMenuBar.add(fileMenu);
+    this.setMenuBar(awtMenuBar);
+
     // set JFrame behavior
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     this.pack();
@@ -155,9 +269,9 @@ public class Logon extends JFrame {
   public static void main(String[] args) throws ParseException, UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
     // Set cross-platform Java L&F (also called "Metal")
-    System.out.println(UIManager.getLookAndFeel());
-    //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
     //System.out.println(UIManager.getLookAndFeel());
+    UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+    System.out.println(UIManager.getLookAndFeel());
 
     GraphicsDevice defaultScreenDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
