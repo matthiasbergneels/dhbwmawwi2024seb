ec85e871229a70f178f43241612aa524b5c8b472
Matthias Berg-Neels <matthias.berg-neels@sap.com>
2025-07-17 18:44:40 +0200

    SortingAlgorithms - BubbleSort

---
+++ src/lecture/chapter13/SortingAlgorithms.java
@@ -0,0 +1,17 @@
+package lecture.chapter13;
+
+public class SortingAlgorithms {
+
+  public static void main(String[] args) {
+
+    int[] toSort = {50, 70, 100, 80, 40, 30, 20, 10 ,60};
+
+
+
+  }
+
+  public static int[] bubbleSort(int[] numbers){
+
+    return numbers;
+  }
+}
