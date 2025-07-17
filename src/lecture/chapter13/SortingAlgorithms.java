20978460b7e5827e898d660f5c4eda893d491ccf
Matthias Berg-Neels <matthias.berg-neels@sap.com>
2025-07-17 18:46:35 +0200

    SortingAlgorithms - BubbleSort

--- src/lecture/chapter13/SortingAlgorithms.java
+++ src/lecture/chapter13/SortingAlgorithms.java
@@ -5,7 +5,12 @@ public class SortingAlgorithms {
   public static void main(String[] args) {
 
     int[] toSort = {50, 70, 100, 80, 40, 30, 20, 10 ,60};
+    System.out.println("Unsortiert: ");
+    printArray(toSort);
 
+    System.out.println("Bubble Sort V1 sortiert: ");
+    int[] sorted = bubbleSort(toSort.clone());
+    printArray(sorted);
 
 
   }
@@ -13,5 +18,21 @@ public class SortingAlgorithms {
   public static int[] bubbleSort(int[] numbers){
 
     return numbers;
+  }
+
+
+
+
+  private static void swap(int[] array, int a, int b) {
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+  }
+
+  public static void printArray(int[] numbers){
+    for(int i = 0; i < numbers.length; i++){
+      System.out.print(numbers[i] + ((i == numbers.length-1)?"" : ", "));
+    }
+    System.out.println();
   }
 }
